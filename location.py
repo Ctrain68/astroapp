b3b430f29ce2ee055a77e532e323992196420cfb
Colin Forster <colinjforster@gmail.com>
2020-09-20 12:19:38 +1000

    first class geolocation

--- location.py
+++ location.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import socket
 import json
 import urllib.request
+import requests
 
 # hostname = socket.gethostname()
 # local_ip = socket.gethostbyname(hostname)
@@ -17,13 +18,65 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-# class Api_location:
-#     def __init__(self, url):
+
+
+class ApiLocation:
+
+
+
+    def __init__(self):
+        self.url = "http://api.ipstack.com/"
+        self.ip = self.get_ip()
+        self.connector = "?access_key="
+        self.key = "60a96c2830ff498d19b2ff0f2de5fd6a"
+        self.location = self.city
+
+
+
+    def get_ip(self):
+        external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+        return external_ip
+
+    
+    def get(self):
+        return requests.get(self.url+self.ip+self.connector+self.key)
+
+    def city(self):
+        data = json.loads(self.get().text)
+        # print(json.loads(data.text))
+        # print(f"This is your city {data['city']}")
+        return data["city"]
+
+
+here = ApiLocation()
+
+here.city()
+ 
+print(here.location())
+
+
+#     response = ""
+
+
+#     def __init__(self):
 #         pass
     
-#     def get_word_from_api(self, ):
-#         request = 
+#     def response(self):
+#         info = requests.get(f"http://api.ipstack.com/60.242.75.5?access_key=60a96c2830ff498d19b2ff0f2de5fd6a") 
+#         return info
+    
+#     def word(self):
+#         data = json.loads(self.response().text)
+#         return (f"This is your city {data['city']}")
+
+
+
+# print(ApiLocation.word())
         
+
+
+
+
    
     
     
