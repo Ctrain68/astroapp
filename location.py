1885a59d469385904acec3321500386f8e914935
Colin Forster <colinjforster@gmail.com>
2020-09-20 10:26:15 +1000

    requirements.txt added

--- location.py
+++ location.py
@@ -1,13 +1,21 @@
-import datetime
+from datetime import datetime 
 import socket
 import json
+import urllib.request
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
+# hostname = socket.gethostname()
+# local_ip = socket.gethostbyname(hostname)
 
-print(local_ip)
+# print(local_ip)
 
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
+print(external_ip)
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
 
 # class Api_location:
 #     def __init__(self, url):
