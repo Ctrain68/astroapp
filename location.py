3330aebad90e0492252d9ee10e9b6f66833222fe
Ubuntu <ubuntu@ip-172-31-34-144.ec2.internal>
2020-09-20 00:10:35 +0000

    psuedo coding

---
+++ location.py
@@ -0,0 +1,36 @@
+import datetime
+import socket
+import json
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+print(local_ip)
+
+
+
+# class Api_location:
+#     def __init__(self, url):
+#         pass
+    
+#     def get_word_from_api(self, ):
+#         request = 
+        
+   
+    
+    
+# class File_handler:
+#      def write_word_to_file(....):
+         
+# class Word:
+#     def ...
+    
+    
+# main:
+#     user input?
+    
+# if:
+#     get_word_from_api
+#     write word to file:
+# # else:
+#     find_wird_in_fi
