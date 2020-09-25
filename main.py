6e666cbb5ebc1f2547eaff45cb7f2c6f8b790eda
Colin Forster <colinjforster@gmail.com>
2020-09-25 12:28:37 +1000

    change print

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from datetime import datetime 
-import socket
-import json
-import urllib.request
-import requests
+
 from location import ApiLocation
 from location import ApiMoon
-import os
-import time
-from sys import stdout
+
 
 greeting = "Welcome to ASTROAPP V1.0"
 
@@ -26,5 +20,3 @@ print(here.city())
 moons = ApiMoon()
 
 print(moons.object_for_date())
- 
-print(moons.object_today())
